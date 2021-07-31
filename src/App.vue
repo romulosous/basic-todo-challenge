2a12a8a1e191e06073f23e152352dd3371291a59
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-07-30 21:38:52 -0300

    basic todo list challenge

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <ul>
+      <li v-for="todo in doneTodos" :key="todo.text">
+        {{ todo.text }}
+      </li>
+    </ul>
+    <button @click="checkAllTodos">Finalizar tudo</button>
+  </div>
 </template>
 
 <script lang="ts">
+/*
+### Checklist
+[x] Definir o dado `todos`
+[x] Preencher os `todos` quando o componente é criado
+[x] Exibir em tela apenas `todos` concluídos
+[x] Ter um método para concluir os `todos`
+[] Mostrar um alerta quando todos os `todos` forem finalizados
+```
+
+### Todos
+[
+  { text: 'Estudar Typescript', done: true },
+  { text: 'Lavar os pratos', done: false },
+  { text: 'Aprender Nuxt.js', done: true }
+]
+```
+*/
+
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+
+interface Todo {
+  text: string;
+  done: boolean;
+}
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      todos: [] as Todo[],
+    };
+  },
+  computed: {
+    doneTodos(): Todo[] {
+      return this.todos.filter((todo) => todo.done);
+    },
+  },
+  watch: {
+    todos(newValue: Todo[]) {
+      const insFinished = !newValue.some(({ done }) => !done);
+      console.log(insFinished);
+      if (insFinished) {
+        alert("Todos as tasks foram finalizados!");
+      }
+    },
+  },
+  created() {
+    this.todos = [
+      { text: "Estudar Typescript", done: true },
+      { text: "Lavar os pratos", done: false },
+      { text: "Aprender Nuxt.js", done: true },
+    ];
+  },
+  methods: {
+    checkAllTodos() {
+      this.todos = this.todos.map(({ text }) => {
+        return { text, done: true };
+      });
+    },
   },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
